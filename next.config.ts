6a559806dbaeeaf651f7246e9280eaf26ef5d2c1
Charles Wefso <61127453+cwefso@users.noreply.github.com>
2025-02-18 15:18:35 -0700

    lighten up next.config

--- next.config.ts
+++ next.config.ts
@@ -5,40 +5,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "rntkgmywnobayfzfandi.supabase.co",
-      },
-      {
-        protocol: "https",
-        hostname: "clerk.accounts.dev",
-      },
-      {
-        protocol: "https",
-        hostname: "artistry.wefsocodes.com", // Updated subdomain
-      },
-      {
-        protocol: "https",
-        hostname: "wefsocodes.com",
-      },
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
+        hostname: "**",
       },
     ],
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `default-src 'self' artistry.wefsocodes.com *.supabase.co clerk.accounts.dev; img-src 'self' data: https:`,
-          },
-        ],
-      },
-    ];
   },
 };
 
